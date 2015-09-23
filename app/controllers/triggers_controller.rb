7994f944fbb761fc4e14c9cdd3252aefe84d8630
Julian Lindner <jl@xq0.net>
2015-09-23 15:28:09 +0200

    added error-handling during API fetches

--- app/controllers/triggers_controller.rb
+++ app/controllers/triggers_controller.rb
@@ -4,22 +4,40 @@ class TriggersController < ApplicationController
   def index
     @user = User.where(name: params[:user_name]).first
     if @user
-      Location.where(user_id: @user.id).each do |l|
-        l.get_weather
+
+      begin
+        Location.where(user_id: @user.id).each do |l|
+          l.get_weather
+        end
+      rescue
+        logger.info("Could not fetch weatherdata.")
       end
 
-      Person.where(user_id: @user.id).each do |p|
-        p.get_activity
-        p.get_health
+      begin
+        Person.where(user_id: @user.id).each do |p|
+          p.get_activity
+          p.get_health
+        end
+      rescue
+        logger.info("Could not fetch activity and health.")
       end
 
-      Newschannel.where(user_id: @user.id).each do |n|
-        n.get_news
+      begin
+        Newschannel.where(user_id: @user.id).each do |n|
+          n.get_news
+        end
+      rescue
+        logger.info("Could not fetch news.")
       end
 
-      Plexserver.where(user_id: @user.id).each do |pm|
-        pm.get_media
+      begin
+        Plexserver.where(user_id: @user.id).each do |pm|
+          pm.get_media
+        end
+      rescue
+        logger.info("Could not fetch plexdata.")
       end
+
     end
   end
 end
