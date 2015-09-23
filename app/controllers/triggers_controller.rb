2806031c9440f2e80bebf996ebcf004c2db13952
Julian Lindner <jl@xq0.net>
2015-09-23 15:37:44 +0200

    added error-handling during API fetches

--- app/controllers/triggers_controller.rb
+++ app/controllers/triggers_controller.rb
@@ -3,41 +3,41 @@ class TriggersController < ApplicationController
 
   def index
     @user = User.where(name: params[:user_name]).first
-    if @user
-
-      begin
-        Location.where(user_id: @user.id).each do |l|
-          l.get_weather
-        end
-      rescue
-        logger.info("Could not fetch weatherdata.")
-      end
-
-      begin
-        Person.where(user_id: @user.id).each do |p|
-          p.get_activity
-          p.get_health
-        end
-      rescue
-        logger.info("Could not fetch activity and health.")
-      end
-
-      begin
-        Newschannel.where(user_id: @user.id).each do |n|
-          n.get_news
-        end
-      rescue
-        logger.info("Could not fetch news.")
-      end
-
-      begin
-        Plexserver.where(user_id: @user.id).each do |pm|
-          pm.get_media
-        end
-      rescue
-        logger.info("Could not fetch plexdata.")
-      end
-
-    end
+#    if @user
+#
+#      begin
+#        Location.where(user_id: @user.id).each do |l|
+#          l.get_weather
+#        end
+#      rescue
+#        logger.info("Could not fetch weatherdata.")
+#      end
+#
+#      begin
+#        Person.where(user_id: @user.id).each do |p|
+#          p.get_activity
+#          p.get_health
+#        end
+#      rescue
+#        logger.info("Could not fetch activity and health.")
+#      end
+#
+#      begin
+#        Newschannel.where(user_id: @user.id).each do |n|
+#          n.get_news
+#        end
+#      rescue
+#        logger.info("Could not fetch news.")
+#      end
+#
+#      begin
+#        Plexserver.where(user_id: @user.id).each do |pm|
+#          pm.get_media
+#        end
+#      rescue
+#        logger.info("Could not fetch plexdata.")
+#      end
+#
+#    end
   end
 end
