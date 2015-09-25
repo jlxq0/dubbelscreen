a886563e077d079e83294002d7219f4ffb5778ab
Julian Lindner <jl@xq0.net>
2015-09-25 12:02:15 +0200

    removed some tasks from jobs

--- app/jobs/get_api_data_for_user_job.rb
+++ app/jobs/get_api_data_for_user_job.rb
@@ -7,36 +7,12 @@ class GetApiDataForUserJob < ActiveJob::Base
     if @user
  
       begin
-        Location.where(user_id: @user.id).each do |l|
-          l.get_weather
-        end
-      rescue
-        logger.info("Could not fetch weatherdata.")
-      end
- 
-      begin
         Person.where(user_id: @user.id).each do |p|
           p.get_activity
           p.get_health
         end
       rescue
         logger.info("Could not fetch activity and health.")
-      end
- 
-      begin
-        Newschannel.where(user_id: @user.id).each do |n|
-          n.get_news
-        end
-      rescue
-        logger.info("Could not fetch news.")
-      end
- 
-      begin
-        Plexserver.where(user_id: @user.id).each do |pm|
-          pm.get_media
-        end
-      rescue
-        logger.info("Could not fetch plexdata.")
       end
  
     end
