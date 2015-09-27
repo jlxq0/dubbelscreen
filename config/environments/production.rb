54e7fcf8cac57d5bf7d388390f1ed6892a68552b
Julian Lindner <jl@xq0.net>
2015-09-27 18:45:15 +0200

    devise seems to work - rudementarily

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.app_domain = 'dubbelscreen.com'
+  config.action_mailer.default_url_options = { host: "dubbelscreen.com", port: 80 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: config.app_domain }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => { omniauth_callbacks: "omniauth_callbacks" }
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   resources :datasources
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root "datasources#index"
+
+  match "/users/:id/finish_signup" => "users#finish_signup", via: [:get, :patch], as: :finish_signup
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
